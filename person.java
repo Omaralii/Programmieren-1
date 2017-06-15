1d099cfe49b9d7f9cf47f8409684ae3b255e8af6
aliomar <aliomar@alis-mbp>
2017-06-16 00:04:06 +0200

    Personen-Klasse hinzugefügt

---
+++ person.java
@@ -0,0 +1,22 @@
+
+public class person {
+	
+	String id;
+	String vorname;
+	String nachname;
+	String alter;
+	
+
+	
+	
+	public person (String vorname,String nachname, String id, String alter)
+	{
+		this.id=id;
+		this.vorname=vorname;
+		this.nachname=nachname;
+		this.alter=alter;
+	}
+	
+	
+	
+}
